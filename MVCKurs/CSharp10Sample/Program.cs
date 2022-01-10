1995e99c7953a072e57e360da4a515874e47c36b
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-01-10 10:32:56 +0100

    Agenda.md global usings (ConsolenAPP) - kommen noch weitere C#10 Features hinzu

---
+++ MVCKurs/CSharp10Sample/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+ //Expliziete Hinzufügen von System.Text
+Console.WriteLine("Hello, World!");
+
+StringBuilder stringBuilder = new StringBuilder();
+
